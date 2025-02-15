645d06fd9bc9ebb938b5a269b046e20de3a70a1e
Ben <ben@Bens-MacBook-Pro.local>
2025-02-15 12:43:04 -0700

    week 06 project v3

--- project/scripts/review-submitted.js
+++ project/scripts/review-submitted.js
@@ -1,3 +1,5 @@
 const reviewCount = parseInt(localStorage.getItem("reviewCount")) ?? 0;
+if ((!typeof reviewCount == Number))
+    reviewCount = 1;
 localStorage.setItem("reviewCount", reviewCount + 1);
 document.querySelector("#review-count").textContent = `You have submitted ${reviewCount + 1} review${reviewCount > 0 ? 's' : ''}!`;
